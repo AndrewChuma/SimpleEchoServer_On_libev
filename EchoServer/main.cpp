a30a1adf46b174f85368809d7d47bf23b493aa47
AndrewChuma <andreychuma.1989@gmail.com>
2024-05-13 20:38:50 +0300

    Add command line params

--- EchoServer/main.cpp
+++ EchoServer/main.cpp
@@ -10,7 +10,7 @@ static void signal_handler(ev::sig &signal, int revents) {
     signal.loop.break_loop();
 }
 
-int main()
+int main(int argc, const char* argv[])
 {
     ev::sig s;
     s.set<signal_handler>();
@@ -19,6 +19,17 @@ int main()
     ev::default_loop loop;
 
     C_Log::getInstance().setup(C_Log::lgrBoth, "sampleLog.log");
+
+    uint16_t port = 5000;
+    std::string address = "";
+
+    if ( argc == 2 ) {
+        address = argv[1];
+    }
+
+    if ( argc == 3 ) {
+        port = std::stoi(argv[2]);
+    }
 
     tcp_conn::C_Server server;
     if ( !server.setup("127.0.0.1", 5000) ) {
